22643f43942c6b0e864bbb5408298dad27f84edd
Jailson Prado <jailsondpd@gmail.com>
2021-11-07 18:23:44 -0300

    Enviando novos arquivos do curso de NODE JS

---
+++ 12_mongoDb/5_resgatando_dados/controllers/ProductController.js
@@ -0,0 +1,26 @@
+const Product = require('../models/Product');
+
+module.exports = class ProductController {
+  static async showProducts(req, res) {
+    const products = await Product.getProduct();
+
+    res.render('products/all', { products });
+  }
+
+  static createProduct(req, res) {
+    res.render('products/create');
+  }
+
+  static createProductPost(req, res) {
+    const name = req.body.name;
+    const image = req.body.image;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    const product = new Product(name, image, price, description);
+    console.log(product);
+
+    product.save();
+    res.redirect('/products');
+  }
+};
---
+++ 12_mongoDb/7_delete/controllers/ProductController.js
@@ -0,0 +1,43 @@
+const Product = require('../models/Product');
+
+module.exports = class ProductController {
+  static async showProducts(req, res) {
+    const products = await Product.getProduct();
+
+    res.render('products/all', { products });
+  }
+
+  static createProduct(req, res) {
+    res.render('products/create');
+  }
+
+  static createProductPost(req, res) {
+    const name = req.body.name;
+    const image = req.body.image;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    const product = new Product(name, image, price, description);
+    console.log(product);
+
+    product.save();
+    res.redirect('/products');
+  }
+
+  static async getProduct(req, res) {
+    const id = req.params.id;
+
+    const product = await Product.getProductById(id);
+
+    res.render('products/product', { product });
+  }
+
+  // deletando dados
+  static async removeProduct(req, res) {
+    const id = req.params.id;
+
+    await Product.removeProductById(id);
+
+    res.redirect('/products');
+  }
+};
